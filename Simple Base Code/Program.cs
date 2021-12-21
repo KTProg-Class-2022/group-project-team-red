515f5bc3bf424bae7805b53d45269bdf2576e875
Ryan De Souza <rdesouza2022@jpkeefehs.org>
2021-12-21 11:51:46 -0500

    mikey naj test

--- Simple Base Code/Program.cs
+++ Simple Base Code/Program.cs
@@ -7,6 +7,7 @@ namespace Simple_Base_Code
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Class!");
+            Console.WriteLine("Mikey Naj");
         }
     }
 }
