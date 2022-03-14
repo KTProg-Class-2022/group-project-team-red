52632ee7bf164df20e059d64235efa1cf8ba787e
m-najarian <80268749+m-najarian@users.noreply.github.com>
2022-03-14 12:17:12 -0400

    one line

--- Simple Base Code/map.cs
+++ Simple Base Code/map.cs
@@ -32,6 +32,7 @@ namespace Simple_Base_Code
                      * Puzzle piece will be found hiding behind the broken frame, next to broken glass
                      * player takes the puzzle piece, completes puzzle, gets a key to leave the room and they walk out of the door
                      */
+
                 }
 
                 if (i == 2)
